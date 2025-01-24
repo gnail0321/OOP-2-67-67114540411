88651cf9e3de2929201830b51fe994ddc8c37e71
gnail0321 <96913734+gnail0321@users.noreply.github.com>
2025-01-24 14:04:08 +0700

    first Create in github
    fuck you

---
+++ app.py
@@ -0,0 +1,11 @@
+class Stu(onject):
+  stu_count = 0
+  def __new__(self):
+    print('Stu.__new__')
+  def __init__(self):
+    print('Stu.__init__')
+    self.gap=4.00
+
+  def study_hard(self):
+    print('Sir, yes sir.')
+  
